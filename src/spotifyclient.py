d01388f1745a9c6d3af08ed46d7255b4e8ab5646
saadshahbaz <59991041+saadshahbaz@users.noreply.github.com>
2021-03-02 13:50:23 +0500

    Update spotifyclient.py

--- src/spotifyclient.py
+++ src/spotifyclient.py
@@ -1,24 +1,33 @@
 import json
+
 import requests
 
 from track import Track
 from playlist import Playlist
 
+
 class SpotifyClient:
+    """SpotifyClient performs operations using the Spotify API."""
 
     def __init__(self, authorization_token, user_id):
-        self.authorization_token = authorization_token
-        self.user_id = user_id
+        """
+        :param authorization_token (str): Spotify API token
+        :param user_id (str): Spotify user id
+        """
+        self._authorization_token = authorization_token
+        self._user_id = user_id
 
-
-    def last_played_tracks(self, limit=10):
+    def get_last_played_tracks(self, limit=10):
+        """Get the last n tracks played by a user
+        :param limit (int): Number of tracks to get. Should be <= 50
+        :return tracks (list of Track): List of last played tracks
+        """
         url = f"https://api.spotify.com/v1/me/player/recently-played?limit={limit}"
         response = self._place_get_api_request(url)
         response_json = response.json()
         tracks = [Track(track["track"]["name"], track["track"]["id"], track["track"]["artists"][0]["name"]) for
                   track in response_json["items"]]
         return tracks
-
 
     def get_track_recommendations(self, seed_tracks, limit=50):
         """Get a list of recommended tracks starting from a number of seed tracks.
@@ -37,7 +46,6 @@ class SpotifyClient:
                   track in response_json["tracks"]]
         return tracks
 
-    
     def create_playlist(self, name):
         """
         :param name (str): New playlist name
@@ -57,8 +65,6 @@ class SpotifyClient:
         playlist = Playlist(name, playlist_id)
         return playlist
 
-
-
     def populate_playlist(self, playlist, tracks):
         """Add tracks to a playlist.
         :param playlist (Playlist): Playlist to which to add tracks
@@ -72,12 +78,15 @@ class SpotifyClient:
         response_json = response.json()
         return response_json
 
-
-
     def _place_get_api_request(self, url):
-        response = requests.post(url,headers={"Content-Type": "application/json","Authorization": f"Bearer {self._authorization_token}"})
+        response = requests.get(
+            url,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {self._authorization_token}"
+            }
+        )
         return response
-
 
     def _place_post_api_request(self, url, data):
         response = requests.post(
